17087a6f104e087dfa236e6b767664ac2a282d55
Flo <f.lefevre22@laposte?net>
2021-10-10 22:19:41 +0200

    Tout OK !

--- app/src/main/java/com/example/puissance11/MenuJeu.java
+++ app/src/main/java/com/example/puissance11/MenuJeu.java
@@ -15,11 +15,16 @@ import androidx.appcompat.widget.AppCompatButton;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MenuJeu extends AppCompatActivity {
@@ -166,6 +171,50 @@ public class MenuJeu extends AppCompatActivity {
 
         if (user != null) {
             user_connecte.setText(user.getEmail());
+
+            Query myTopPostsQuery = mDatabase.orderByChild("score");
+            myTopPostsQuery.addChildEventListener(new ChildEventListener() {
+                long classement =0;
+
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                    long Nombre_total = snapshot.getChildrenCount ();
+                    if (classement==0) {
+                        classement = Nombre_total+1;
+                    }
+
+                    System.out.println("The " + snapshot.getKey() + " score is " + snapshot.getValue()+ "classement = "+classement);
+
+                    Map<String, Object> childUpdates = new HashMap<>();
+                    childUpdates.put(snapshot.getKey()+"/rang/",classement);
+                    mDatabase.updateChildren(childUpdates);
+                    classement--;
+
+                }
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+
+                }
+
+            });
+
             readScore(new MyCallback() {
                 @Override
                 public void onCallback(Long value) {
